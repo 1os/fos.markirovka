401127c9ec864e5df1c6931646cd6ce48f9244ed
Lazarev Eugene <lazareveugene@gmail.com>
2024-09-17 14:54:02 +0500

    init

---
+++ fos.markirovka/Program.cs
@@ -0,0 +1,141 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web;
+using System.IO;
+
+public class Program
+{
+    public static void handleOpenURL(string[] args)
+    {
+        File.WriteAllLines(Path.GetTempPath() + Guid.NewGuid().ToString() + ".txt", Environment.GetCommandLineArgs());
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Приложение обработчик печати этикеток честный знак");
+
+
+        if (args.Length > 0 && args[0].IndexOf("fosmarkirovka:") == 0)
+        {
+            //lp -o fit-to-page -o media=Custom.58x40mm -d Xprinter_XP_365B km.png
+            var query = args[0].Split('?')[1];
+            var parms = query.Split('&');
+            var media = "";
+            var printer = "";
+            var url = "";
+            var filename = "";
+            var arguments = "";
+            var filepath = Path.GetTempPath() + Guid.NewGuid().ToString() + ".png";
+            foreach (var parm in parms)
+            {
+                var toupl = parm.Split('=');
+                switch (toupl[0])
+                {
+                    case "media":
+                        media = HttpUtility.UrlDecode(toupl[1]);
+                        break;
+                    case "printer":
+                        printer = HttpUtility.UrlDecode(toupl[1]);
+                        break;
+                    case "url":
+                        url = HttpUtility.UrlDecode(toupl[1]);
+                        File.WriteAllBytes(filepath, Convert.FromBase64String(url));
+                        break;
+                }
+            }
+            filename = "lp";
+            arguments = string.Format("-o fit-to-page -o media=Custom.{1}mm -d {2} km.png", media, printer);
+
+            Console.Write(filename + " " + arguments);
+            using (Process process = new Process())
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.FileName = filename;
+                startInfo.Arguments = arguments;
+                process.StartInfo = startInfo;
+                process.Start();
+                do
+                {
+                    if (!process.HasExited)
+                    {
+                        process.Refresh();
+                    }
+                }
+                while (!process.WaitForExit(1000));
+                Console.WriteLine("exit code: " + process.ExitCode);
+                Console.WriteLine("Отправили на печать этикетку");
+            }
+            using (Process process = new Process())
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.FileName = "osascript";
+                startInfo.Arguments = "-e 'display notification \"Отправили на печать этикетку\" with title \"1ОС.Маркировка\"  sound name \"Submarine\"'";
+                process.StartInfo = startInfo;
+                process.Start();
+                do
+                {
+                    if (!process.HasExited)
+                    {
+                        process.Refresh();
+                    }
+                }
+                while (!process.WaitForExit(1000));
+            }
+            // Thread.Sleep(5);
+            // File.Delete(filepath);
+        }
+        else
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                RegistryKey key;
+                key = Registry.ClassesRoot.CreateSubKey("fosmarkirovka");
+                key.SetValue("", "URL: fosmarkirovka Protocol");
+                key.SetValue("URL Protocol", "");
+
+                key = key.CreateSubKey("shell");
+                key = key.CreateSubKey("open");
+                key = key.CreateSubKey("command");
+                key.SetValue("", Process.GetCurrentProcess().MainModule.FileName + " \"%1\"");
+                key.Close();
+
+                Console.WriteLine("Успешно настроили приложение для работы Windows.");
+                Console.ReadKey();
+            }
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Console.WriteLine("Успешно настроили приложение для работы Linux.");
+            }
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Console.WriteLine("Успешно настроили приложение для работы OSX.");
+                //"osascript -e 'tell app \"System Events\" to display dialog \"Успешно настроили приложение для работы OSX.\"'"        
+                using (Process process = new Process())
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    startInfo.FileName = "osascript";
+                    startInfo.Arguments = "-e 'display notification \"Успешно настроили приложение для работы OSX.\" with title \"1ОС.Маркировка\"  sound name \"Submarine\"'";
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    do
+                    {
+                        if (!process.HasExited)
+                        {
+                            process.Refresh();
+                        }
+                    }
+                    while (!process.WaitForExit(1000));
+                }
+            }
+        }
+    }
+}
