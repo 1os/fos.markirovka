b3b8ead32deaa741c4e92e83995e03051ab296a2
lazareveugene <lazareveugene@gmail.com>
2024-09-24 18:50:19 +0500

    ...

--- buildSnap.sh
+++ buildSnap.sh
@@ -3,11 +3,11 @@ rm fos-markirovka_2024.09.20_arm64.snap
 rm fos-markirovka_2024.09.20_armhf.snap
 rm fos-markirovka_2024.09.20_amd64.snap
 
-dotnet publish --configuration Release --runtime linux-arm64 --self-contained
-dotnet publish --configuration Release --runtime linux-arm --self-contained
-dotnet publish --configuration Release --runtime linux-amd64 --self-contained
+#dotnet publish --configuration Release --runtime linux-arm64 --self-contained
+#dotnet publish --configuration Release --runtime linux-arm --self-contained
+#dotnet publish --configuration Release --runtime linux-amd64 --self-contained
 
-snapcraft clean
+#snapcraft clean
 snapcraft
 
 sudo snap install --devmode --edge fos-markirovka_2024.09.20_arm64.snap
