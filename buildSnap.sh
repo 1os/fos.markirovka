f05abafe28787a0fa9096276e3e87c83981de68a
lazareveugene <lazareveugene@gmail.com>
2024-09-25 11:10:34 +0300

    2024.09.25

--- buildSnap.sh
+++ buildSnap.sh
@@ -6,7 +6,9 @@ rm fos-markirovka_2024.09.20_amd64.snap
 #dotnet publish fos.markirovka.csproj --configuration Release --self-contained
 
 #snapcraft clean
-snapcraft
+snapcraft --target-arch arm64
+snapcraft --target-arch armhf
+snapcraft --target-arch amd64
 
 sudo snap install --devmode --edge fos-markirovka_2024.09.20_arm64.snap
 sudo snap install --devmode --edge fos-markirovka_2024.09.20_armhf.snap
--- buildWin.bat
+++ buildWin.bat
@@ -1,5 +1,16 @@
 @ECHO OFF
 
-dotnet publish fos.markirovka.csproj --configuration Release --runtime win-arm64 --self-contained
-dotnet publish fos.markirovka.csproj --configuration Release --runtime win-amd64 --self-contained
-dotnet publish fos.markirovka.csproj --configuration Release --runtime win-x86 --self-contained
+chcp 65001
+
+dotnet publish fos.markirovka.csproj --configuration Release --runtime win-arm64 --self-contained -p:PublishSingleFile=true
+dotnet publish fos.markirovka.csproj --configuration Release --runtime win-amd64 --self-contained -p:PublishSingleFile=true
+dotnet publish fos.markirovka.csproj --configuration Release --runtime win-x86 --self-contained -p:PublishSingleFile=true
+
+move bin\Release\net8.0\win-x64\publish\fos.markirovka.exe bin\Release\net8.0\win-x64\publish\fos.markirovka.x64.exe
+tar -a -c -f "1ОС.Маркировка ЧестныйЗнак.x64.exe.zip" bin\Release\net8.0\win-x64\publish\fos.markirovka.x64.exe
+
+move bin\Release\net8.0\win-arm64\publish\fos.markirovka.exe bin\Release\net8.0\win-arm64\publish\fos.markirovka.arm64.exe
+tar -a -c -f "1ОС.Маркировка ЧестныйЗнак.arm64.exe.zip" bin\Release\net8.0\win-arm64\publish\fos.markirovka.arm64.exe
+
+move bin\Release\net8.0\win-x86\publish\fos.markirovka.exe bin\Release\net8.0\win-x86\publish\fos.markirovka.win32.exe
+tar -a -c -f "1ОС.Маркировка ЧестныйЗнак.win32.exe.zip" bin\Release\net8.0\win-x86\publish\fos.markirovka.win32.exe
